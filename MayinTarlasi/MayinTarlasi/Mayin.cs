9250a220275f89d0ffcc95cfa15cfce81c36a76a
yusufyldrm44 <147240891+yusufyldrm44@users.noreply.github.com>
2025-01-13 09:42:21 +0300

    Add files via upload

---
+++ MayinTarlasi/MayinTarlasi/Mayin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MayinTarlasi
+{
+    public class Mayin
+    {
+        Point loc;
+        bool dolu;
+        bool bakildi;
+        public Mayin(Point loca)
+        {
+            dolu = false;
+            loc = loca;
+        }
+        public Point konum_al
+        {
+            get { return loc; }
+        }
+        public bool mayin_varmi
+        {
+            get { return dolu; }
+            set { dolu = value; }
+        }
+        public bool bakildi_
+        {
+            get { return bakildi; }
+            set { bakildi = value; }
+        }
+
+
+
+
+    }
+}
